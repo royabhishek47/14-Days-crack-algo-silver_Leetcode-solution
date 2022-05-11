9a8176d2b5003036500107de00e3f5aee9309df5
ABHISHEK ROY <98046344+royabhishek47@users.noreply.github.com>
2022-05-11 10:06:51 +0530

    Add files via upload

---
+++ Reverse linked list.java
@@ -0,0 +1,14 @@
+//Reverse linked list
+
+class Solution {
+  public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
